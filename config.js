ad15e9b4d418ac31a6e1635b6f2f32428328a110
patty2843 <patty2843@gmail.com>
2026-09-24 16:26:10 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.IVY_CONFIG = {
-  SUPABASE_URL: ' https://alrgwyyowyfjtfsthywa.supabase.co ',
-  SUPABASE_ANON_KEY: ' sb_publishable_8CSfpEZRPfI-2-nYL4H_Og_WCr5Cj1c '
+  SUPABASE_URL: 'https://alrgwyyowyfjtfsthywa.supabase.co/rest/v1/',
+  SUPABASE_ANON_KEY: 'sb_publishable_8CSfpEZRPfI-2-nYL4H_Og_WCr5Cj1c',
+  VAPID_PUBLIC_KEY: 'YOUR_WEB_PUSH_PUBLIC_VAPID_KEY'
 };
